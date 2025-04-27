9d954d52dc278482f1cd2f35db642e3c436522ed
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2025-04-27 23:55:36 +0200

    added task into navigation

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,10 +1,10 @@
 from .login_view import router as login_router 
 from .user_view import router as user_router
-from .tenant_user_view import router as tenant_user_router
+from .task_view import router as task_router
 
 routers = [
     login_router,
-    tenant_user_router,
-    user_router
+    user_router,
+    task_router
 ]
 
