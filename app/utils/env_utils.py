406b3cc28601f4239413a19462a87f998f0737c1
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2025-05-11 23:57:13 +0200

    Add env utils module

---
+++ app/utils/env_utils.py
@@ -0,0 +1,28 @@
+from enum import Enum
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path=".env")
+
+class EnvironmentVariable(str, Enum):
+    """Enumeration of all environment variables used in the application."""
+
+   
+    DB_HOST = "DB_HOST"
+    DB_NAME = "DB_NAME"
+    DB_PASSWORD = "DB_PASSWORD" 
+    DB_USERNAME = "DB_USERNAME"
+    DB_URL = "DB_URL"
+    
+
+    SECRET_KEY = "SECRET_KEY"
+    ALGORITHM = "ALGORITHM"
+    ACCESS_TOKEN_EXPIRE_MINUTES = "ACCESS_TOKEN_EXPIRE_MINUTES"
+    
+    HOST = "HOST"
+    PORT = "PORT"
+
+def get_env(key: EnvironmentVariable, default: str = None) -> str:
+    """Get environment variable value."""
+    return os.getenv(key.value, default)
