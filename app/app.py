546a48aa90e795e24b925436117bc348df21e30a
darisdr <darisdragusha@gmail.com>
2025-05-14 14:19:18 +0200

    removed custom_openapi

--- app/app.py
+++ app/app.py
@@ -132,31 +132,6 @@ app.add_middleware(
 for router in routers:
     app.include_router(router)
 
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    openapi_schema = get_openapi(
-        title="FastAPI",
-        version="0.1.0",
-        description="API with JWT Bearer authentication",
-        routes=app.routes,
-    )
-    openapi_schema["components"]["securitySchemes"] = {
-        "BearerAuth": {
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT"
-        }
-    }
-    for path in openapi_schema["paths"].values():
-        for operation in path.values():
-            operation.setdefault("security", []).append({"BearerAuth": []})
-
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-app.openapi = custom_openapi
 
 if __name__ == "__main__":
     import uvicorn
