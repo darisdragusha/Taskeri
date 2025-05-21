af53aa7ef220f75ea7f47563b65f17d2dd717345
darisdr <darisdragusha@gmail.com>
2025-05-21 20:17:43 +0200

    refactored user endpoints to use prefix

--- app/views/user_view.py
+++ app/views/user_view.py
@@ -7,9 +7,9 @@ from app.utils.db_utils import get_db
 from app.auth import auth_service
 
 
-router = APIRouter(tags=["User"])
+router = APIRouter(prefix="/users", tags=["User"])
 
-@router.post("/users/create", response_model=UserResponse)
+@router.post("/create", response_model=UserResponse)
 async def create_user(
     user_create: UserCreate,
     request: Request,
@@ -23,7 +23,7 @@ async def create_user(
     return controller.create_user(user_create, current_user)
 
 
-@router.get("/users/{user_id}", response_model=UserResponse)
+@router.get("/{user_id}", response_model=UserResponse)
 async def get_user(
     user_id: int,
     request: Request,
@@ -44,7 +44,7 @@ async def get_user(
     
     return controller.get_user(user_id)
 
-@router.get("/users", response_model=List[UserResponse])
+@router.get("", response_model=List[UserResponse])
 async def get_all_users(
     request: Request,
     controller: UserController = Depends(),
@@ -63,7 +63,7 @@ async def get_all_users(
 
 
 
-@router.put("/users/{user_id}", response_model=UserResponse)
+@router.put("/{user_id}", response_model=UserResponse)
 async def update_user(
     user_id: int,
     user_update: UserUpdate,
@@ -84,7 +84,7 @@ async def update_user(
     """
     return controller.update_user(user_id, user_update)
 
-@router.delete("/users/{user_id}", response_model=dict)
+@router.delete("/{user_id}", response_model=dict)
 async def delete_user(
     user_id: int,
     request: Request,
